3869bab2f9a8e91354a8d51855169e8d3d20c209
TakkMai <ivanoval.ru@yandex.ru>
2022-09-03 11:32:26 +0200

    шаг 5

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ const costsFlat = document.getElementById("costs-flat"),
       costsHouseServise = document.getElementById("costs-house-services"),
       costsTransport = document.getElementById("costs-transport"),
       costsCredit = document.getElementById("costs-credit");
+
+      //total inputs
+const totalDayInput = document.getElementById("total-day"),
+      totalMonthInput = document.getElementById("total-month"),
+      totalYearInput = document.getElementById("total-year");
+
+let totalDay, totalMonth, totalYear;
+
+//money box
+const moneyBoxRange = document.getElementById("money-box-range"),
+      accumulationInput = document.getElementById("accumulation"),
+      spend = document.getElementById("spend");
+
+let accumulation = 0;
+let totalPrecents = 0;      
