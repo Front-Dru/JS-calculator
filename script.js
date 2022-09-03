538b5e83319f0f48f8e6ea0fc2b63672ab647a0c
TakkMai <ivanoval.ru@yandex.ru>
2022-09-03 11:18:14 +0200

    шаг4

---
+++ script.js
@@ -0,0 +1,11 @@
+//income inputs
+const incomeSalary = document.getElementById("income-salary"),
+      incomeFreelance = document.getElementById("income-freelace"),
+      incomeExtra1 = document.getElementById("income-extra1"),
+      incomeExtra2 = document.getElementById("income-extra2");
+
+      //cost inputs
+const costsFlat = document.getElementById("costs-flat"),
+      costsHouseServise = document.getElementById("costs-house-services"),
+      costsTransport = document.getElementById("costs-transport"),
+      costsCredit = document.getElementById("costs-credit");
